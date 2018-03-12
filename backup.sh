bf9ff695e1c7a8ff813b79956c2345a17bc531bd
Alex Coyle <abcoyle@gmail.com>
2018-03-12 15:27:17 +0000

    Support paths with whitespace chars

--- backup.sh
+++ backup.sh
@@ -17,15 +17,27 @@ script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
 mkdir -p ${TMPDIR}
 
-for dir in ${DIRS}; do \
-  { cd $dir && \
-    mkdir ${MEDIA}/$(basename $dir).tmp && \
-    $script_dir/7za.expect ${PASSWORD} a -p ${OPTS} -w${TMPDIR} ${MEDIA}/$(basename $dir).tmp/$(basename $dir).7z . && \
-    rm -rf ${MEDIA}/$(basename $dir) && \
-    mv ${MEDIA}/$(basename $dir).tmp ${MEDIA}/$(basename $dir); } || exit $?; \
+# https://unix.stackexchange.com/questions/102891/posix-compliant-way-to-work-with-a-list-of-filenames-possibly-with-whitespace
+set -f; IFS='
+'
+for dir in ${DIRS}; do
+  set +f; unset IFS
+  { cd "$dir" && \
+    mkdir ${MEDIA}/"$(basename "$dir")".tmp && \
+    $script_dir/7za.expect ${PASSWORD} a -p ${OPTS} -w${TMPDIR} ${MEDIA}/"$(basename "$dir")".tmp/"$(basename "$dir")".7z . && \
+    rm -rf ${MEDIA}/"$(basename "$dir")" && \
+    mv ${MEDIA}/"$(basename "$dir")".tmp ${MEDIA}/"$(basename "$dir")"; } || exit $?
 done
+set +f; unset IFS
 
 # TODO following should be in a trap
 
-for dir in ${DIRS}; do rm -rf ${MEDIA}/$(basename $dir).tmp; done
+#set -f; IFS='
+#'
+#for dir in ${DIRS}; do
+#  set +f; unset IFS
+#  rm -rf ${MEDIA}/"$(basename "$dir")".tmp
+#done
+#set +f; unset IFS
+
 rm -rf ${TMPDIR}
